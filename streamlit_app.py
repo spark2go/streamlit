31c3c27075b35433583c5dc26760e980d508a3f0
spark2go <106140919+spark2go@users.noreply.github.com>
2023-05-30 14:11:56 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,15 @@
 # init
 import streamlit as sl
+import pandas as pd
+
+# var
+f = 'https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt'
 
 sl.text('hello')
+lst = pd.read_csv(f)
+
+lst = lst.set_index('Fruit')
+sl.multiselect('pick:', list(lst.index))
+sl.dataframe(lst)
+
+### EOL
